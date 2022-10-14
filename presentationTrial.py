a2c2dbe84cdcf3d61dc9f94d8e6667ad138075b4
Keren <amanyakaren1@gmail.com>
2022-10-14 21:24:58 +0300

    Format return as percentage

--- presentationTrial.py
+++ presentationTrial.py
@@ -1,6 +1,6 @@
 def Calculate_Return(beginningVal,endingVal):
 	IG = endingVal - beginningVal
-	IG_Growth = IG / beginningVal
+	IG_Growth = "{:.2%}".format(IG / beginningVal)
 	return IG_Growth
 
 InvestmentGrowth(2000000,4000000)
